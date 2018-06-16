ab3dc1ab739d96e05a1bc8fa1cf77c1d74188e92
Petr Koveshnikov <rjdgtn@gmail.com>
2018-06-16 16:27:28 +0300

    SmsStorage save

--- android-studio/app/src/main/java/rjdgtn/csms/SmsStorage.java
+++ android-studio/app/src/main/java/rjdgtn/csms/SmsStorage.java
@@ -9,15 +9,42 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class SmsStorage {
-    static void saveSms(Context context/*, int kind, int id, long date, String number, String text*/) {//} throws IOException {
-//        if (kind == Telephony.TextBasedSmsColumns.MESSAGE_TYPE_INBOX) {
-//
-//        }
+    static void save(Context context, int id, int kind, long date, String number, String text) {//} throws IOException {
+        boolean isInbox = (kind == Telephony.TextBasedSmsColumns.MESSAGE_TYPE_INBOX);
+        try {
+            File file = new File(Environment.getExternalStorageDirectory()+ "/CSMS/history.txt");
+            if (!file.exists()) {
+                File directory = new File(Environment.getExternalStorageDirectory() + "/CSMS");
+                directory.mkdirs();
+                file.createNewFile();
+            }
+
+            FileWriter out = new FileWriter(file, true);
+            String dateStr = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(new Date(date));
+
+            out.write("\n");
+            out.write((isInbox ? "in " : "out ") + Integer.toString(id) + "\n");
+            out.write(dateStr + "\n");
+            out.write(number + "\n");
+            out.write(text + "\n");
+
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (isInbox) {
+            updateMaxSmsInboxId(context, id);
+        }
     }
 
     static int getMaxSmsInboxId() {
@@ -46,7 +73,7 @@ public class SmsStorage {
                     directory.mkdirs();
                     file.createNewFile();
                 }
-                PrintWriter out = new PrintWriter(file);
+                FileWriter out = new FileWriter(file);
                 out.write(Integer.toString(id));
                 out.close();
             } catch (IOException e) {
